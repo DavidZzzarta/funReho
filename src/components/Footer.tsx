e8da049baeddb830e5a0d2c5d85cf4d6fc778051
DavidZzzarta <jhottfriend@gmail.com>
2026-07-31 04:23:04 -0700

    2026-07-31 04:23

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -1,150 +1,84 @@
+import { SocialIcon } from "react-social-icons";
+
+function SocialIcons() {
+  return (
+    <>
+      <SocialIcon url="https://facebook.com/couetilc" />
+      <SocialIcon url="https://instagram.com/couetilc" />
+      <SocialIcon url="https://youtube.com/couetilc" />
+      <SocialIcon url="mailto:contacto@fundacion.org" />
+      <SocialIcon url="https://wa.me/573000000000" />
+    </>
+  );
+}
+
 export function Footer() {
   return (
-    <footer class="bg-white">
-      <div class="container mx-auto px-8">
-        <div class="w-full flex flex-col md:flex-row py-6">
-          <div class="flex-1 mb-6 text-black">
-            <a
-              class="text-pink-600 no-underline hover:no-underline font-bold text-2xl lg:text-4xl"
-              href="#"
-            >
-              <svg
-                class="h-8 fill-current inline"
-                xmlns="http://www.w3.org/2000/svg"
-                viewBox="0 0 512.005 512.005"
-              >
-                <rect
-                  fill="#2a2a31"
-                  x="16.539"
-                  y="425.626"
-                  width="479.767"
-                  height="50.502"
-                  transform="matrix(1,0,0,1,0,0)"
-                />
-                <path
-                  class="plane-take-off"
-                  d=" M 510.7 189.151 C 505.271 168.95 484.565 156.956 464.365 162.385 L 330.156 198.367 L 155.924 35.878 L 107.19 49.008 L 211.729 230.183 L 86.232 263.767 L 36.614 224.754 L 0 234.603 L 45.957 314.27 L 65.274 347.727 L 105.802 336.869 L 240.011 300.886 L 349.726 271.469 L 483.935 235.486 C 504.134 230.057 516.129 209.352 510.7 189.151 Z "
-                />
-              </svg>
-              Fundacion Rehoboth
-            </a>
+    <footer className="bg-white border-t">
+      <div className="container mx-auto px-8 py-12">
+        <div className="flex flex-col gap-10 md:flex-row md:justify-between">
+          {/* Logo / Información */}
+          <div className="max-w-sm">
+            <h2 className="text-2xl font-bold text-black">
+              Fundación Rehoboth
+            </h2>
+            <p className="mt-3 text-gray-600">
+              Transformando vidas a través de la educación, el apoyo social y el
+              desarrollo comunitario.
+            </p>
+
+            <div className="mt-6 flex gap-3">
+              <SocialIcons />
+            </div>
           </div>
-          <div class="flex-1">
-            <p class="uppercase text-gray-500 md:mb-6">Links</p>
-            <ul class="list-reset mb-6">
-              <li class="mt-2 inline-block mr-2 md:block md:mr-0">
+
+          {/* Navegación */}
+          <div>
+            <h3 className="mb-4 text-lg font-semibold text-black">Conócenos</h3>
+
+            <ul className="space-y-2">
+              <li>
                 <a
-                  href="#"
-                  class="no-underline hover:underline text-gray-800 hover:text-pink-500"
+                  href="/mision-vision"
+                  className="hover:underline text-gray-600"
                 >
-                  FAQ
+                  Misión y visión
                 </a>
               </li>
-              <li class="mt-2 inline-block mr-2 md:block md:mr-0">
+
+              <li>
                 <a
-                  href="#"
-                  class="no-underline hover:underline text-gray-800 hover:text-pink-500"
+                  href="/plan-accion"
+                  className="hover:underline text-gray-600"
                 >
-                  Help
+                  Plan de acción
                 </a>
               </li>
-              <li class="mt-2 inline-block mr-2 md:block md:mr-0">
-                <a
-                  href="#"
-                  class="no-underline hover:underline text-gray-800 hover:text-pink-500"
-                >
-                  Support
+
+              <li>
+                <a href="/recorrido" className="hover:underline text-gray-600">
+                  Recorrido
                 </a>
               </li>
             </ul>
           </div>
-          <div class="flex-1">
-            <p class="uppercase text-gray-500 md:mb-6">Legal</p>
-            <ul class="list-reset mb-6">
-              <li class="mt-2 inline-block mr-2 md:block md:mr-0">
-                <a
-                  href="#"
-                  class="no-underline hover:underline text-gray-800 hover:text-pink-500"
-                >
-                  Terms
-                </a>
-              </li>
-              <li class="mt-2 inline-block mr-2 md:block md:mr-0">
-                <a
-                  href="#"
-                  class="no-underline hover:underline text-gray-800 hover:text-pink-500"
-                >
-                  Privacy
-                </a>
-              </li>
-            </ul>
-          </div>
-          <div class="flex-1">
-            <p class="uppercase text-gray-500 md:mb-6">Social</p>
-            <ul class="list-reset mb-6">
-              <li class="mt-2 inline-block mr-2 md:block md:mr-0">
-                <a
-                  href="#"
-                  class="no-underline hover:underline text-gray-800 hover:text-pink-500"
-                >
-                  Facebook
-                </a>
-              </li>
-              <li class="mt-2 inline-block mr-2 md:block md:mr-0">
-                <a
-                  href="#"
-                  class="no-underline hover:underline text-gray-800 hover:text-pink-500"
-                >
-                  Linkedin
-                </a>
-              </li>
-              <li class="mt-2 inline-block mr-2 md:block md:mr-0">
-                <a
-                  href="#"
-                  class="no-underline hover:underline text-gray-800 hover:text-pink-500"
-                >
-                  Twitter
-                </a>
-              </li>
-            </ul>
-          </div>
-          <div class="flex-1">
-            <p class="uppercase text-gray-500 md:mb-6">Company</p>
-            <ul class="list-reset mb-6">
-              <li class="mt-2 inline-block mr-2 md:block md:mr-0">
-                <a
-                  href="#"
-                  class="no-underline hover:underline text-gray-800 hover:text-pink-500"
-                >
-                  Official Blog
-                </a>
-              </li>
-              <li class="mt-2 inline-block mr-2 md:block md:mr-0">
-                <a
-                  href="#"
-                  class="no-underline hover:underline text-gray-800 hover:text-pink-500"
-                >
-                  About Us
-                </a>
-              </li>
-              <li class="mt-2 inline-block mr-2 md:block md:mr-0">
-                <a
-                  href="#"
-                  class="no-underline hover:underline text-gray-800 hover:text-pink-500"
-                >
-                  Contact
-                </a>
-              </li>
+
+          {/* Contacto */}
+          <div>
+            <h3 className="mb-4 text-lg font-semibold text-black">Contacto</h3>
+
+            <ul className="space-y-2">
+              <li className="text-gray-600">Email: contacto@fundacion.org</li>
+              <li className="text-gray-600">Tel: +57 300 000 0000</li>
+              <li className="text-gray-600">Bogotá, Colombia</li>
             </ul>
           </div>
         </div>
+
+        <div className="mt-10 border-t pt-6 text-center text-sm text-gray-500">
+          © {new Date().getFullYear()} Fundación. Todos los derechos reservados.
+        </div>
       </div>
-      <a
-        href="https://www.freepik.com/free-photos-vectors/background"
-        class="text-gray-500"
-      >
-        Background vector created by freepik - www.freepik.com
-      </a>
     </footer>
   );
 }
